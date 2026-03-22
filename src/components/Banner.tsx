66e9450fd7b9ccb0cc6fc81906ba93763d69dcba
vhgbyyy <gaby.lucasdeoliveira@gmail.com>
2026-03-22 13:44:13 -0300

    banner e navbar att

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,16 +2,36 @@ import bannerImg from "../assets/banner.png";
 
 function Banner() {
   return (
-    <section 
-      className="banner" 
-      style={{ 
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${bannerImg})` 
-      }}
-    >
-      <div className="banner-content">
-        <h1>REALIZA ANTICHAMAS - AGENTE EXTINTOR DE INCÊNDIO</h1>
-        <p> Nanotecnologia para salvar vidas e patrimônios</p>
-        <button className="btn-orcamento">Solicitar orçamento</button>
+    <section className="banner">
+      <div className="banner-container">
+        
+        <div className="banner-text">
+          <h1 className="titulo-banner">
+  <span className="linha1">A Revolução Nanotecnológica no</span>
+  <span className="linha2"> Combate a Incêndios</span>
+</h1>
+
+          <p className="subtitle">
+            Tecnologia limpa, sustentável e até <br />
+            <strong>10x mais eficiente que a água.</strong>
+          </p>
+
+          <ul className="solutions-list">
+            <li>Soluções inovadoras para proteger vidas, patrimônios e meio ambiente. florestais e florestais.</li>
+            <li>Soluções inovadoras para proteger vidas, patrimônios, industriais em industriais e urbanos.</li>
+          </ul>
+
+          <div className="buttons">
+            <button className="btn-saiba-mais">Saiba mais</button>
+            <button className="btn-orcamento">Solicitar orçamento</button>
+          </div>
+
+        </div>
+
+        <div className="banner-image">
+          <img src={bannerImg} alt="Tecnologia nanotecnológica no combate a incêndios" />
+        </div>
+
       </div>
     </section>
   );
